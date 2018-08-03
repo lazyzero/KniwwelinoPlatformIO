006514dfb1e30f0c9cc571a4fb94a61743cc9766
Christian Moll <christian.moll@list.lu>
2018-08-03 09:23:38 +0200

    Upladed library list for KniwwelinoLib 1.10.0

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 
 void setup() {
     Kniwwelino.begin(true, true, false);
+    if (WiFi.status() != WL_CONNECTED) {
+      WiFi.beginWPSConfig();
+      delay(3000);
+    }
 }
 
 void loop() {
     Kniwwelino.loop();
-		Kniwwelino.sleep(250);
+    Kniwwelino.sleep(250);
 }
