80c798981981854acd44803f26649fa06b045bb2
Christian Moll <christian.moll@list.lu>
2018-08-03 09:27:39 +0200

    removed code that was introduced by accident

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,8 @@
 
 void setup() {
     Kniwwelino.begin(true, true, false);
-    if (WiFi.status() != WL_CONNECTED) {
-      WiFi.beginWPSConfig();
-      delay(3000);
-    }
 }
 
 void loop() {
     Kniwwelino.loop();
-    Kniwwelino.sleep(250);
 }
